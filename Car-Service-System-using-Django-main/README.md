59d37c231cd696d25e8742ddade66d08b3c63dbb
Anvita Mahajan <78889572+Anvita0305@users.noreply.github.com>
2022-06-09 16:28:21 +0530

    Delete Car-Service-System-using-Django-main directory

--- Car-Service-System-using-Django-main/README.md
+++
@@ -1,7 +0,0 @@
-# Car-Service-System-using-Django
-Django based Management system
-
-
-Authors :
-Chinmayee Kulkarni 112003074,
-Anvita Mahajan 112003080
--- Car-Service-System-using-Django-main/logsys/logsys/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS='True'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='mahajananvita3@gmail.com'
-EMAIL_HOST_PASSWORD='rppoopprojsem4'
-EMAIL_PORT=587
