8418b82b6742be5bb1e255f04182eaf173e2637a
Anvita Mahajan <78889572+Anvita0305@users.noreply.github.com>
2022-06-21 08:29:23 +0530

    Delete Car-Service-System-using-Django-main directory

--- Car-Service-System-using-Django-main/logsys/authenticate/admin.py
+++
@@ -1,9 +0,0 @@
-import imp
-from operator import contains
-from django.contrib import admin
-from.models import Appoint, Contact
-# Register your models here.
-
-admin.site.register(Appoint),
-admin.site.register(Contact),
-# admin.site.register(Feedback)
